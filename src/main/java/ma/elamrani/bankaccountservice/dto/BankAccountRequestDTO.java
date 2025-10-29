f005922558e3ab8c1d7f68a3d4e0bd4cb9738fe8
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-10-29 21:40:51 +0100

    feat(dto): add DTOs and mapper for BankAccount entity
    - Create BankAccountRequestDTO and BankAccountResponseDTO
    - Add AccountMapper for mapping between entity and DTOs

---
+++ src/main/java/ma/elamrani/bankaccountservice/dto/BankAccountRequestDTO.java
@@ -0,0 +1,18 @@
+package ma.elamrani.bankaccountservice.dto;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ma.elamrani.bankaccountservice.enums.AccountType;
+
+import java.util.Date;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class BankAccountRequestDTO {
+    private Double balance;
+    private String currency;
+    private AccountType type;
+}
